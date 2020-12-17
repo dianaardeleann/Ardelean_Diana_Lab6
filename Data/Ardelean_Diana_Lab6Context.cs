c4192102c8f60c58d29023bb78325f2a5a304f46
DIANA-TEODORA ARDELEAN <diana.teodora.ardelean@stud.ubbcluj.ro>
2020-12-17 09:11:06 +0200

    Add project files.

---
+++ Data/Ardelean_Diana_Lab6Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ardelean_Diana_Lab6.Models;
+
+namespace Ardelean_Diana_Lab6.Data
+{
+    public class Ardelean_Diana_Lab6Context : DbContext
+    {
+        public Ardelean_Diana_Lab6Context (DbContextOptions<Ardelean_Diana_Lab6Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ardelean_Diana_Lab6.Models.Book> Book { get; set; }
+    }
+}
